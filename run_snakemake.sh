f518da9efd0d0b136cf03114d48d82a0932eae08
shamlymajeed <shamlymajeed08@gmail.com>
2024-10-25 10:10:27 +0200

    make a job script. update config/testcase_2.json to check more files.

---
+++ run_snakemake.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -f
+
+#$ -N KAPy 
+#$ -l h_rt=24:00:00
+#$ -l h_rss=3G,mem_free=3G
+#$ -S /bin/bash
+#$ -q all.qls
+#$ -M shamlym@met.no
+#$ -m bae
+#$ -o /lustre/storeC-ext/users/klimakverna/development/jobs/OUT_$JOB_NAME.$JOB_ID
+#$ -e /lustre/storeC-ext/users/klimakverna/development/jobs/ERR_$JOB_NAME.$JOB_ID
+#$ -R y
+echo "*** Starting ***"
+source /modules/centos7/conda/prod_04_2021/etc/profile.d/conda.sh
+conda activate /lustre/storeC-ext/users/klimakverna/development/conda/KAPy
+echo "*** Running snakemake ***"
+snakemake --cores=3 --keep-going
+echo "*** Done ***"
